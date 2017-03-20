a56c822831863a7f319a88d3926634af3be5a11e
Daniil Kogtev <oclaw68@gmail.com>
2017-03-21 00:03:55 +0300

    adding REST API functions prototypes

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ app.listen(8080, () =>  {
 	console.log('Server is up!');
 });
 
+//Админская страничка
 app.get('/', (req, res) => sendHtml(res,'./view/admin.html'));
 
+//Метод заказа преподавателем отчета
 app.get('/report/:id/:group', (req,res) => {
 	if (reporter.sendReport(req.params.id,
 		req.params.group)) {
@@ -19,6 +21,44 @@ app.get('/report/:id/:group', (req,res) => {
 		res.send('Something happened, we can`t create your report');
 	}
 });
+
+// CRUD методы для работы с базой данных
+app.post('/students', (req,res) => {
+	//Здесь создаем нового студента
+});
+
+app.post('/teachers', (req,res) => {
+	//Здесь создаем нового преподавателя
+})
+
+app.post('/groups', (req,res) =>{
+	//Здесь создаем новую группу
+})
+
+app.delete('/groups/:id', (req,res) => {
+	//Здесь удаляем группу
+})
+
+app.delete('/students/:id', (req,res) => {
+	//Здесь удаляем студента
+})
+
+app.delete('/teachers/:id', (req,res) => {
+	//Здесь удаляем преподавателя
+})
+
+app.put('/groups/:id', (req,res) => {
+	//Здесь изменяем группу
+})
+
+app.put('/students/:id', (req,res) => {
+	// Здесь изменяем студента
+	// Основной метод получения геолокационных данных от студента
+})
+
+app.put('/teachers/:id', (req,res) => {
+	//Здесь изменяем преподавателя
+})
 
 function sendHtml(res, pathToHtml) {
 	filesystem.readFile(pathToHtml, (err,html) =>{
